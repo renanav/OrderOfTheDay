8a6a6d5bf31e2e073fb1d03c354b0e0341feb9f0
Renan Avrahami <renan.avrahami@gmail.com>
2017-12-21 21:42:38 -0500

    Initial Commit

---
+++ OrderOfTheDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrderOfTheDay
+//
+//  Created by Renan Avrahami 2017 on 12/21/17.
+//  Copyright © 2017 Renan Avrahami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
